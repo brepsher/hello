6f8bf05da1a51e0b199767d38d5d1e1c18c9d8ea
Brian <brian.repsher@arrisi.com>
2013-10-28 15:20:48 -0500

    Servlet

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -15,6 +15,6 @@ public class HelloServlet extends HttpServlet {
     {
         // Very simple - just return some plain text
         PrintWriter writer = response.getWriter();
-        writer.print("Hello World");
+        writer.print("<h1>Hello Worldi</h1>");
     }
 }
